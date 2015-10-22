fe870da4871b5b0650f79cd18ddc7d8dbd7d9c06
Albert Liu <albertliu@naturalwellbeing.com>
2015-10-22 15:37:21 -0700

    update pr phone

--- app/overrides/add_power_reviews_snippet_to_product.rb
+++ app/overrides/add_power_reviews_snippet_to_product.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => 'spree/products/show',
   :name => 'add_power_reviews_snippet_to_product',
-  :insert_before => "div[data-hook='product_subtitle_main']",
+  :insert_before => "div[data-hook='product_title']",
   :partial => "spree/shared/powerreviews_product_snippet")

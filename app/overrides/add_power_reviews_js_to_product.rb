1397cb8406346bdf5e83660dded2f757b7616dc5
Albert Liu <albertliu@naturalwellbeing.com>
2016-01-13 14:23:42 -0800

    pwr js loaded in product page now

--- app/overrides/add_power_reviews_js_to_product.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/products/show',
-  :name => 'add_power_reviews_js_to_product',
-  :insert_after => "div[data-hook='product_subtitle_main']",
-  :partial => "spree/shared/powerreviews_engine_js")

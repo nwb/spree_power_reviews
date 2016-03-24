c71d96801a5e77b667ef8de9bc6efd6ef93be7b8
Albert Liu <albertliu@naturalwellbeing.com>
2016-03-23 17:57:48 -0700

    no qa from power reviews anymore

--- app/overrides/add_qa_to_product.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/products/show',
-  :name => 'add_qa_to_product',
-  :insert_after => "div[data-hook='product_directions']",
-  :partial => "spree/shared/powerreviews_product_qas")

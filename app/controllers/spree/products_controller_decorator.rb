14adff031ed6922c988bfa92782ac097023f1056
Albert Liu <albertliu@naturalwellbeing.com>
2016-03-28 15:40:56 -0700

    leave review product in product controller

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -3,7 +3,18 @@ Spree::ProductsController.class_eval do
   before_action :load_product, only: [:review, :question, :answer]
 
   def review
-
+    @review_product=@product
+    if %w(1738 1739 1740).include?(@product.id.to_s)
+      @review_product=Spree::Product.find(1737)
+    end
+    #fo-ti
+    if %w(1770).include?(@product.id.to_s)
+      @review_product=Spree::Product.find(1439)
+    end
+    #ksg
+    if %w(1839).include?(@product.id.to_s)
+      @review_product=Spree::Product.find(1077)
+    end
   end
 
   def question

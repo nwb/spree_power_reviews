542cc4663f2d9be54f82fbe7c548288048c559ae
Albert Liu <albertliu@naturalwellbeing.com>
2017-05-18 13:20:54 -0700

    review should not care product available or not

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,8 +1,9 @@
 Spree::ProductsController.class_eval do
 
-  before_action :load_product, only: [:review, :question, :answer]
+  #before_action :load_product, only: [:question, :answer]
 
   def review
+    @product=Spree::Product.friendly.find(params[:id])
     @review_product=@product
     if %w(1738 1739 1740).include?(@product.id.to_s)
       @review_product=Spree::Product.find(1737)
@@ -21,10 +22,11 @@ Spree::ProductsController.class_eval do
   end
 
   def question
-
+    @product=Spree::Product.friendly.find(params[:id])
   end
 
   def answer
+    @product=Spree::Product.friendly.find(params[:id])
     render "spree/products/review"
   end
 
